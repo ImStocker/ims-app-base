6bcb6d6f0ae5bb95f600baa018b521641525f08a
vika <vikanurieva2003@gmail.com>
2026-06-09 16:00:44 +0400

    Fixed blocks definitions

--- app/components/Asset/ProjectTree/ProjectWorkspacesPresenter.vue
+++ app/components/Asset/ProjectTree/ProjectWorkspacesPresenter.vue
@@ -79,7 +79,7 @@ export default defineComponent({
   components: {
     TreePresenter,
     ProjectTreePresenterWorkspace,
-    ProjectTreePresenterAdditional
+    ProjectTreePresenterAdditional,
   },
   props: {
     selection: {
